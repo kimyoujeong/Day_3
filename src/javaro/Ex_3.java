47143e0d872ad29f38f432aa91e47883a80ede82
kimyoujeong <55773434+kimyoujeong@users.noreply.github.com>
2021-01-01 20:35:12 +0900

    원의 둘레,원의 면적

---
+++ src/javaro/Ex_3.java
@@ -0,0 +1,14 @@
+package javaro;
+public class Ex_3{
+	static double PI=3.141592;
+	public static void main(String[] args) {
+		System.out.println("변수");
+		int r=15;
+		System.out.println("원주율"+PI+",반지름"+r+"cm인 원의 둘레는"+2*PI*r+"cm입니다");
+		System.out.println("2진수:"+Integer.toBinaryString(r));
+		System.out.println("8진수:"+Integer.toOctalString(r));
+		System.out.println("16진수"+Integer.toHexString(r));
+		double rd = 15.3;
+		System.out.println("원주율"+PI+"반지름"+r+"cm인 원의 면적은"+r*PI*r+"cmcm입니다");
+	}
+}
